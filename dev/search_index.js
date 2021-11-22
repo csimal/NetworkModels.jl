956946dec1f139fb6e67db40054a8248b06f15c8
Documenter.jl <documenter@juliadocs.github.io>
2021-11-22 12:38:54 +0000

    build based on 2230ebc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NetworkModels","category":"page"},{"location":"#NetworkModels","page":"Home","title":"NetworkModels","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NetworkModels.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NetworkModels]","category":"page"}]
+}
